78488e72f3b99fa5d9541b1aac3915d268641948
rizafairuzzabadi <fairuzzabadi17@itu.edu.tr>
2022-09-27 11:08:02 +0700

    Before going

--- from_scratch/lib/main.dart
+++ from_scratch/lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(const MyApp());
-}
+
+// void main(){
+//   runApp(const MyApp());
+// }
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
+  @override //a decorator. Deliberately overriding the default behavior of the build method
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Text('Hello!'),); //named arguments
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('My First App'),
+          ),
+          body: const Text('Hello World!')),
+    ); //Scaffold is a widget that provides a basic layout
   }
 }
