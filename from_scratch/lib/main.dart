fac33fb897d5f7e98c48a778a723c6414d9a7738
rizafairuzzabadi <fairuzzabadi17@itu.edu.tr>
2022-09-28 11:34:30 +0700

    End of 2nd Day

--- from_scratch/lib/main.dart
+++ from_scratch/lib/main.dart
@@ -7,16 +7,29 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
+  void answerQuestion(){
+    print('Answer chosen!');
+  }
+
+  const MyApp({super.key}); //key is a property of the stateless widget     
+
+  
   @override //a decorator. Deliberately overriding the default behavior of the build method
   Widget build(BuildContext context) {
+    var questions = ['What\'s your favorite color?', 'What\'s your favorite animal?'];
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             title: const Text('My First App'),
           ),
-          body: const Text('Hello World!')),
+          body: Column(children: [
+            const Text('The question!'),
+            ElevatedButton(onPressed: answerQuestion, child: const Text('Answer 1')), 
+            ElevatedButton(onPressed: answerQuestion, child: const Text('Answer 2')),
+            ElevatedButton(onPressed: answerQuestion, child: const Text('Answer 3')),
+          ],), //Widget is a class
+          ),
     ); //Scaffold is a widget that provides a basic layout
   }
 }
